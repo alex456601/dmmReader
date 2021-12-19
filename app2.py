50f2194f5e5bfae4850fd10aac656e4e1c4c02f0
alex456601 <alex456601@gmail.com>
2021-12-19 21:29:43 +0100

    new version 1

--- app2.py
+++ app2.py
@@ -1,13 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+
 import pyvisa
 from time import sleep
 from datetime import datetime
+
 
 app = Flask(__name__)
 
 ergebnis = None
 
-@app.route('/', methods = ['POST', 'GET'])
+
+
+
+
+@app.route('/')
 
 def messung():
 
@@ -17,11 +23,10 @@ def messung():
 
     time = datetime.now().strftime("%H,%M,%S")
     date = datetime.now().strftime("%Y,%m,%d")
+    zeitpunkt= datetime.now().strftime("%Y.%m.%d-%H:%M:%S")
 
-
-    #dmm.write("SYST:TIME " + time)
-    #dmm.write("SYST: " + date)
-    #dmm.write("SYST:TIME 15,00,05")
+    dmm.write("SYST:TIME " + time)
+    dmm.write("SYST:DATE " + date)
 
     dmm.timeout = 5000
     dmm.write("*CLS")
@@ -30,9 +35,9 @@ def messung():
     scanIntervals = 0
     numberScans = 1
     channelDelay = 0.1
-    scanlist = "(@101:103)"
+    scanlist = "(@101:105)"
 
-    dmm.write("CONF:TEMP TC,T,(@101:103)")
+    dmm.write("CONF:TEMP TC,T,(@101:105)")
 
     dmm.write("ROUTE:SCAN " + scanlist)
     dmm.write("ROUTE:SCAN:SIZE?")
@@ -44,6 +49,7 @@ def messung():
     dmm.write("TRIG:SOUR TIMER")
     dmm.write("TRIG:TIMER " + str(scanIntervals))
     dmm.write("INIT;:SYSTEM:TIME:SCAN?")
+    leeren()
     #ausgabe vom Zeitpunkt der Messung
     print(dmm.read())
 
@@ -53,27 +59,36 @@ def messung():
         dmm.write("DATA:POINTS?")
         points = int(dmm.read())
 
-    #jedes ergebnis hat eine Zeile
-    for chan in range(1, 100):
+
+    #\n after every data
+    print("Werte:")
+    for chan in range(1, numberChannels + 1):
         dmm.write("DATA:REMOVE? 1")
-        raw = str(dmm.read())      #ergebnise
-        print(raw[0:2] + raw[4] + "." + raw[5:8] + "°" + " " + raw[-4:-1])
-        points = 0
+        raw = str(dmm.read())  # ergebnisse
+        with open("static/output.csv", "a") as csv_file:
+            print(str([raw[0:2] + raw[4] + "." + raw[5:8], raw[-4:-1], zeitpunkt]))
+            data = str([raw[0:2] + raw[4] + "." + raw[5:8], raw[-4:-1], zeitpunkt])
+            csv_file.write("%s" % data + "\n")
+            csv_file.close()
+        points = 1
+
 
         #wait for data
         while (points == 0):
             dmm.write("DATA:POINTS?")
             points = int(dmm.read())
 
-    return str(ergebnis) and reset()
 
-def reset():
-    rm = pyvisa.ResourceManager('@py')
-    dmm = rm.open_resource('TCPIP0::192.168.178.101::inst0::INSTR')
-    dmm.write("*CLS")
-    dmm.write("*RST")
+
+
     dmm.close()
-    print("Verbindung unterbrochen")
+    return str(ergebnis)
+
+def leeren():
+
+    with open("static/output.csv", "w") as csv_file:
+        csv_file.write("")
+        csv_file.close()
     return
 
 
